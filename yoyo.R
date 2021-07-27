82502eaaa88f5a054953ae0dc6e8b902c39de383
siardv <84980377+siardv@users.noreply.github.com>
2021-07-27 06:23:27 +0200

    Update yoyo.R

--- yoyo.R
+++ yoyo.R
@@ -1,32 +1,40 @@
-# measure yo-yo effect (or weight cycling) with BMI from panel data
+## measure yo-yo effect (weight cycling) with panel data
 
-yoyo <- function(panel, pid, waves, bmi) {
-  df <- na.omit(data.frame(panel[, c(pid, waves, bmi)], yoyo = 0))
-  pid <- type.convert(names(which(table(df[, 1]) >= 3))) # select participants with minimum 3 measures
+yoyo <- function(panel, pid, waves, bmi, append = TRUE) {
+  df <- na.omit(data.frame(panel[, c(pid, waves, bmi)], yoyo = 0))  # select id's that occur in at least 3 waves
+  df <- df[order(df[, 2]), ]
+  pid <- type.convert(names(which(table(df[, 1]) >= 3)))  # generate all combinations of size 3 for set of waves
   w <- unique(df[, 2])
-  if (length(w) <= 3)
-    stop("Minimum 3 time points required.")
-  w <- expand.grid(w, w, w, stringsAsFactors = FALSE) # all combinations of size 3 for set of waves where w1 < w2 < w3
+  if (length(w) < 3) 
+    stop("Minimum of 3 time points required.")
+  w <- expand.grid(w, w, w, stringsAsFactors = FALSE)
   w <- subset(w, w[, 1] < w[, 2] & w[, 2] < w[, 3])
   cat("\n")
   for (p in seq_along(pid)) {
-    cat("\r Progress :", format(round(p / length(pid) * 100, 2), nsmall = 2), "%") # progress
-    sub <- df[df[1] == pid[p], ]
-    wp <- w[which(apply(w, 1, function(x) all(x %in% sub[, 2]))), ] # get all valid combinations for participant p
-    wp <- data.frame(cbind(wp[, 3], wp), row.names = NULL) # reference wave: highest of 3
-    wp[, 2:4] <- sapply(2:4, function(i) sub[, 3][match(wp[, i], unique(wp[, i]))]) # add corresponding BMI
-    yy <- which((wp[, 2] > wp[, 3] & wp[, 3] < wp[, 4]) | (wp[, 2] < wp[, 3] & wp[, 3] > wp[, 4])) # detect yo-yo patterns: down-up | up-down
-    if (length(yy) > 0){
-      wp[yy, 5] <- sapply(seq_along(yy), function(i) mean(abs(diff(as.numeric(wp[i, 2:4]))))) # yo-yo mean per wave
-      sapply(unique(wp[, 1]), function(i) df[df[, 1] %in% pid[p] & df[, 2] == i, 4] <<- mean(wp[wp[, 1] == i, 5], na.rm = TRUE)) # add score to main data frame
+    cat("\r Progress :", format(round(p/length(pid) * 100, 2), nsmall = 2), "%")  # progress
+    sub <- df[df[1] == pid[p], ]  # select valid wave combinations for participant p
+    wp <- w[which(apply(w, 1, function(x) all(x %in% sub[, 2]))), ]  # get all wave combinations in which participant p participated
+    wp <- data.frame(cbind(wp[, 3], wp), row.names = NULL)  # reference wave: highest of trio
+    wp[, 2:4] <- sapply(2:4, function(i) sub[, 3][match(wp[, i], unique(wp[, i]))])  # add corresponding BMI score
+    yy <- which((wp[, 2] > wp[, 3] & wp[, 3] < wp[, 4]) | (wp[, 2] < wp[, 3] & wp[, 3] > wp[, 4]))  # detect yo-yo patterns: down-up | up-down
+    if (length(yy) > 0) {
+      wp[yy, 5] <- sapply(seq_along(yy), function(i) mean(abs(diff(as.numeric(wp[i, 2:4])))))  # yo-yo mean per wave
+      sapply(unique(wp[, 1]), function(i) wp[wp[, 1] == i, 5] <<- mean(wp[wp[, 1] == i, 5], na.rm = TRUE))
+      wp <- unique(wp[, c(1, 5)])
+      wp[, 2][!is.finite(wp[, 2])] <- 0
+      df[which(df[, 1] == pid[p] & df[, 2] %in% wp[, 1]), 4] <- wp[, 2]
     }
   }
+  if (append == TRUE)
+    df <- merge(panel, df, by = names(panel)[c(pid, waves, bmi)])
   return(df)
 }
 
-# example
+## example
 yy <- yoyo(panel = df, pid = 1, waves = 3, bmi = 4)
+             
 # panel = data structure containing repeated measures data
 # pid = individual-level identifier (column index)
 # waves = wave/time variable (column index)
 # bmi = measured bmi (column index)
+# append = return source data with appended yo-yo column
