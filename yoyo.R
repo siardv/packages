10dd39cee14bd065a1f0555994462ece9bba0bae
siardv <84980377+siardv@users.noreply.github.com>
2021-08-08 13:07:01 +0200

    Update yoyo.R

--- yoyo.R
+++ yoyo.R
@@ -12,25 +12,25 @@ yoyo <- function(panel, id, waves, bmi, append = FALSE) {
   for (i in seq_along(ids)) {
     tryCatch({
       cat("\r Progress: ", format(round(i/length(ids) * 100, 2), nsmall = 2), "%\t", sep = "")
-      sub <- df[df[1] == ids[i], ]   # valid wave combinations for participant i
+      sub <- df[df[1] == ids[i], ]   # relevant  wave combinations
       if (any(table(sub[2]) > 1))
         warn <- 1
       wp <- w[which(apply(w, 1, function(x) all(x %in% sub[, 2]))), ]
-      wp <- data.frame(cbind(wp[, 3], wp))   # add reference wave: (1, 3, 4) -> (4, 1, 3, 4)
-      wp[, 2:4] <- sapply(2:4, function(x) sub[, 3][match(wp[, x], unique(wp[, x]))]) # add BMI: (4, 1, 3, 4) -> (4, 21.7, 22.1, 20.2)
+      wp <- data.frame(cbind(wp[, 3], wp))   # add reference wave: highest of 3-element subset (1, 3, 4) -> (4, 1, 3, 4)
+      wp[, 2:4] <- sapply(2:4, function(x) sub[, 3][match(wp[, x], unique(wp[, x]))]) # add corresponding BMI score (4, 21, 22, 21)
       yy <- which((wp[, 2] > wp[, 3] & wp[, 3] < wp[, 4]) | (wp[, 2] < wp[, 3] & wp[, 3] > wp[, 4]))   # detect yo-yo patterns: down-up | up-down
       if (length(yy) > 0) {
         wp[yy, 5] <- sapply(yy, function(x) mean(abs(diff(as.numeric(wp[x, 2:4])))))   # yo-yo mean per wave
         sapply(unique(wp[, 1]), function(x) wp[wp[, 1] == x, 5] <<- ifelse(all(is.na(wp[wp[, 1] == x, 5])), 0, mean(wp[wp[, 1] == x, 5], na.rm = TRUE)))
         wp <- unique(wp[, c(1, 5)])
-        apply(wp, 1, function(r) df[which(df[, 2] == r[1] & df[, 1] == ids[i]), 4])
+        apply(wp, 1, function(x) df[which(df[, 2] == x[1] & df[, 1] == ids[i]), 4] <<- x[2])
       }
     }, error = function(e){})
   }
   if (append == TRUE)
     df <- merge(panel, df, by = names(panel)[c(id, waves, bmi)])
-  if (exists("warn"))
-    cat("\n Some ids have multiple measures per wave: only first values are used.\n")
+  if(exists("warn"))
+    cat("\n Some ids have multiple measures in the same wave: only first values are used.\n")
   return(df)
 }
 
