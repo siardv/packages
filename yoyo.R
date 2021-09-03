7415796439abc4bd825b329a166ebbc68ecc0f9c
siardv <84980377+siardv@users.noreply.github.com>
2021-09-03 21:28:33 +0200

    Update yoyo.R

--- yoyo.R
+++ yoyo.R
@@ -15,11 +15,11 @@ yoyo <- function(panel, id, waves, bmi, append = FALSE) {
       if (any(table(sub[2]) > 1))
         warn <- 1
       wp <- w[which(apply(w, 1, function(x) all(x %in% sub[, 2]))), ]   # subsets for i
-      wp <- data.frame(cbind(wp[, 3], wp))   # add reference wave: (1, 3, 4) -> (4, 1, 3, 4)
+      wp <- data.frame(cbind(wp[, 3], wp))   # add outcome/reference wave: (1, 3, 4) -> (4, 1, 3, 4)
       wp[, 2:4] <- sapply(2:4, function(x) sub[, 3][match(wp[, x], unique(wp[, x]))]) # add BMI: (4, 1, 3, 4) -> (4, 21.2, 22.8, 20.9)
       yy <- which((wp[, 2] > wp[, 3] & wp[, 3] < wp[, 4]) | (wp[, 2] < wp[, 3] & wp[, 3] > wp[, 4]))   # detect yo-yo patterns: down-up | up-down
       if (length(yy) > 0) {
-        wp[yy, 5] <- sapply(yy, function(x) mean(abs(diff(as.numeric(wp[x, 2:4])))))   # yo-yo mean per wave
+        wp[yy, 5] <- sapply(yy, function(x) mean(abs(diff(as.numeric(wp[x, 2:4])))))   # yo-yo mean per reference wave
         sapply(unique(wp[, 1]), function(x) wp[wp[, 1] == x, 5] <<- ifelse(all(is.na(wp[wp[, 1] == x, 5])), 0, mean(wp[wp[, 1] == x, 5], na.rm = TRUE)))
         wp <- unique(wp[, c(1, 5)])
         apply(wp, 1, function(x) df[which(df[, 2] == x[1] & df[, 1] == ids[i]), 4] <<- x[2])
@@ -38,8 +38,8 @@ yoyo <- function(panel, id, waves, bmi, append = FALSE) {
 # id        Individual-level identifier
 # waves     Wave/time variable (positive integer)
 # bmi       Measured BMI
-# append    Logical, if FALSE (default) a data frame with columns specified in arguments with added yo-yo column is returned;
-#                    if TRUE, a original data frame from panel-argument with added yo-yo column is returned.
+# append    Logical, if FALSE (default) a four column data frame is returned containing the columns specified in the arguments plus a column containing the yo-yo score;
+#                    if TRUE, the original data frame plus a column containing the yo-yo score is returned.
 
 ## Example
-yy <- yoyo(panel = df, id = 1, waves = 2, bmi = 3)
+yoyo_score <- yoyo(panel = df, id = 1, waves = 2, bmi = 3)
